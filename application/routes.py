9067254cb085716c2db5143b570d793f64377840
Yomudogly <khalnepesov@gmail.com>
2020-03-20 10:44:21 -0400

    added additional endpoints to for multiple search matches

--- application/routes.py
+++ application/routes.py
@@ -14,7 +14,7 @@ from flask_jwt_simple import jwt_required, create_jwt, get_jwt_identity
     
 ###### PRODUCTS #####
 
-@api.route('/products')
+@api.route('/products', '/products/' )
 class ProductsGetAll(Resource):
     
     # GET ALL
@@ -29,26 +29,92 @@ class ProductsGetAll(Resource):
 @api.route('/products/<int:idx>')
 class ProductById(Resource):
     
-    # GET ONE
+    # GET ONE BY INDEX
     # @jwt_required
     @cache.cached(timeout=50)
     def get(self, idx: int):
         return jsonify(Products.objects(id=idx))
     
+ 
     
-@api.route('/products/<txt>+<txt_1>')
-class ProductBy2Names(Resource):
+@api.route('/products/<txt>')
+class ProductByWord(Resource):
     
-    # GET ONE
+    # GET PRODUCTS SEARCH BY 1 MATCH IN NAME
     # @jwt_required
     @cache.cached(timeout=50)
-    def get(self, txt: str, txt_1: str):
+    def get(self, txt: str):
     
         resp = Products.objects.aggregate(*[
             {
                 '$match': {
                     'name': {
                         '$regex': '.*' + txt + '.*', 
+                        '$options':'i'
+                    }
+                }
+            }
+        ])
+        
+        resp_string = encoder.encode(list(resp))
+        
+        return json.loads(resp_string), 200
+    
+    
+@api.route('/products/<txt>+<txt_>')
+class ProductBy2Words(Resource):
+    
+    # GET PRODUCTS SEARCH BY 2 MATCHES IN NAME
+    # @jwt_required
+    @cache.cached(timeout=50)
+    def get(self, txt: str, txt_: str):
+    
+        resp = Products.objects.aggregate(*[
+            {
+                '$match': {
+                    'name': {
+                        '$regex': '.*' + txt + '.*', 
+                        '$options':'i'
+                    }
+                }
+            },
+            {
+                '$match': {
+                    'name': {
+                        '$regex': '.*' + txt_ + '.*', 
+                        '$options':'i'
+                    }
+                }
+            }
+        ])
+        
+        resp_string = encoder.encode(list(resp))
+        
+        return json.loads(resp_string), 200
+    
+    
+
+@api.route('/products/<txt>+<txt_>+<txt_1>')
+class ProductBy3Words(Resource):
+    
+    # GET PRODUCTS SEARCH BY 3 MATCHES IN NAME
+    # @jwt_required
+    @cache.cached(timeout=50)
+    def get(self, txt: str, txt_: str, txt_1: str):
+    
+        resp = Products.objects.aggregate(*[
+            {
+                '$match': {
+                    'name': {
+                        '$regex': '.*' + txt + '.*', 
+                        '$options':'i'
+                    }
+                }
+            },
+            {
+                '$match': {
+                    'name': {
+                        '$regex': '.*' + txt_ + '.*', 
                         '$options':'i'
                     }
                 }
@@ -67,20 +133,44 @@ class ProductBy2Names(Resource):
         
         return json.loads(resp_string), 200
     
-
-@api.route('/products/<name>')
-class ProductByName(Resource):
     
-    # GET ONE
+@api.route('/products/<txt>+<txt_>+<txt_1>+<txt_2>')
+class ProductBy4Words(Resource):
+    
+    # GET PRODUCTS SEARCH BY 4 MATCHES IN NAME
     # @jwt_required
     @cache.cached(timeout=50)
-    def get(self, name: str):
+    def get(self, txt: str, txt_: str, txt_1: str, txt_2: str):
     
         resp = Products.objects.aggregate(*[
             {
                 '$match': {
                     'name': {
-                        '$regex': '.*' + name + '.*', 
+                        '$regex': '.*' + txt + '.*', 
+                        '$options':'i'
+                    }
+                }
+            },
+            {
+                '$match': {
+                    'name': {
+                        '$regex': '.*' + txt_ + '.*', 
+                        '$options':'i'
+                    }
+                }
+            },
+            {
+                '$match': {
+                    'name': {
+                        '$regex': '.*' + txt_1 + '.*', 
+                        '$options':'i'
+                    }
+                }
+            },
+            {
+                '$match': {
+                    'name': {
+                        '$regex': '.*' + txt_2 + '.*', 
                         '$options':'i'
                     }
                 }
@@ -90,21 +180,53 @@ class ProductByName(Resource):
         resp_string = encoder.encode(list(resp))
         
         return json.loads(resp_string), 200
-  
-  
-@api.route('/products/slug/<slug>')
-class ProductBySlug(Resource):
     
-    # GET ONE
+    
+@api.route('/products/<txt>+<txt_>+<txt_1>+<txt_2>+<txt_3>')
+class ProductBy5Words(Resource):
+    
+    # GET PRODUCTS SEARCH BY 5 MATCHES IN NAME
     # @jwt_required
     @cache.cached(timeout=50)
-    def get(self, slug: str):
+    def get(self, txt: str, txt_: str, txt_1: str, txt_2: str, txt_3: str):
     
         resp = Products.objects.aggregate(*[
             {
                 '$match': {
-                    'slug': {
-                        '$regex': '.*' + slug + '.*', 
+                    'name': {
+                        '$regex': '.*' + txt + '.*', 
+                        '$options':'i'
+                    }
+                }
+            },
+            {
+                '$match': {
+                    'name': {
+                        '$regex': '.*' + txt_ + '.*', 
+                        '$options':'i'
+                    }
+                }
+            },
+            {
+                '$match': {
+                    'name': {
+                        '$regex': '.*' + txt_1 + '.*', 
+                        '$options':'i'
+                    }
+                }
+            },
+            {
+                '$match': {
+                    'name': {
+                        '$regex': '.*' + txt_2 + '.*', 
+                        '$options':'i'
+                    }
+                }
+            },
+            {
+                '$match': {
+                    'name': {
+                        '$regex': '.*' + txt_3 + '.*', 
                         '$options':'i'
                     }
                 }
@@ -114,6 +236,31 @@ class ProductBySlug(Resource):
         resp_string = encoder.encode(list(resp))
         
         return json.loads(resp_string), 200
+    
+  
+  
+# @api.route('/products/slug/<slug>')
+# class ProductBySlug(Resource):
+    
+#     # GET ONE
+#     # @jwt_required
+#     @cache.cached(timeout=50)
+#     def get(self, slug: str):
+    
+#         resp = Products.objects.aggregate(*[
+#             {
+#                 '$match': {
+#                     'slug': {
+#                         '$regex': '.*' + slug + '.*', 
+#                         '$options':'i'
+#                     }
+#                 }
+#             }
+#         ])
+        
+#         resp_string = encoder.encode(list(resp))
+        
+#         return json.loads(resp_string), 200
 
         
     
